dbdf89fd1adc7b5290b8af78704f43f49f1fe451
Adrien Bertrand <bertrand.adrien@gmail.com>
2023-12-06 19:22:56 +0100

    Add "built" branch, create specific readme.

--- scripts/__init__.py
+++
@@ -1,4 +0,0 @@
-from .parse import Token, Tokens, OsVersion, OsVersions, Translation
-from .trie import TokenTrie
-
-__all__ = ["Token", "Tokens", "OsVersion", "OsVersions", "Translation", "TokenTrie"]
